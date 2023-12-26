9246f89f4cedf8dbee3097d1f6dab6d39d7b2b9a
Mehmet Başgöze <mhmtbsgz34@gmail.com>
2023-12-26 18:02:24 +0300

    Ön İşleme Şablonu Commit Edildi

---
+++ Veri Ön İşleme/onIslemeSablonu.py
@@ -0,0 +1,52 @@
+#1.kutuphaneler
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#2.veri onisleme
+#2.1.veri yukleme
+veriler = pd.read_csv('satislar.csv')
+#test
+print(veriler)
+#veri on isleme
+
+aylar = veriler[['Aylar']]
+print(aylar)
+
+satislar = veriler[['Satislar']]
+print(satislar)
+
+satislar2 = veriler.iloc[:,:1].values
+#print(satislar2)
+
+#verilerin egitim ve test icin bolunmesi
+from sklearn.model_selection import train_test_split
+
+x_train, x_test,y_train,y_test = train_test_split(aylar,satislar,test_size=0.33, random_state=0)
+
+#verilerin olceklenmesi
+from sklearn.preprocessing import StandardScaler
+
+sc=StandardScaler()
+
+X_train = sc.fit_transform(x_train)
+X_test = sc.fit_transform(x_test)
+
+Y_train = sc.fit_transform(y_train)
+Y_test = sc.fit_transform(y_test)
+
+#Model İnşası (Linear Regression)
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(x_train, y_train)
+
+tahmin = lr.predict(x_test)
+
+# Verileri Görselleştirme (Lineer Regresyon ile) 
+x_train = x_train.sort_index()
+y_train = y_train.sort_index()
+plt.plot(x_train, y_train)
+plt.plot(x_test, lr.predict(x_test))
+
+
+
